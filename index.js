dbd6f58097189b8fac2a5e479a3452124a9021f7
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:49 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ instance.prototype.init_presets = function () {
 instance.prototype.actions = function (system) {
   var self = this;
 
-  self.system.emit("instance_actions", self.id, {
+  self.setActions({
     PON: { label: "Power ON" },
     POF: { label: "Power OFF" },
     AVL: {
